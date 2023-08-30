d73ac2c29a69f1a71a50b447aa97fd4d7e0a5b71
alberbendayan <108437189+alberbendayan@users.noreply.github.com>
2023-08-30 17:01:20 -0300

    Child q calcula el MD5 de un archivo x param

---
+++ child.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/md5.h>
+
+#define READ_BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <nombre_del_archivo>\n", argv[0]);
+        return 1;
+    }
+
+    const char *filename = argv[1];
+    FILE *file = fopen(filename, "rb");
+
+    if (!file) {
+        printf("No se pudo abrir el archivo.\n");
+        return 1;
+    }
+
+    MD5_CTX md5Context;
+    MD5_Init(&md5Context);
+
+    unsigned char buffer[READ_BUFFER_SIZE];
+    size_t bytesRead;
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        MD5_Update(&md5Context, buffer, bytesRead);
+    }
+
+    unsigned char hash[MD5_DIGEST_LENGTH];
+    MD5_Final(hash, &md5Context);
+    // en hash queda guardado el md5
+    fclose(file);
+
+    printf("El MD5 del archivo %s es: ", filename);
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        printf("%02x", hash[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
