f7c893fbb5b97a6161639c397918cc7bbb377b46
alberbendayan <108437189+alberbendayan@users.noreply.github.com>
2023-09-06 15:30:55 -0300

    Open files

--- child.c
+++ child.c
@@ -1,19 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <openssl/md5.h>
 
 #define READ_BUFFER_SIZE 1024
 
-void makeMD5 (char *argv[]);
+void makeMD5 (char *argv);
 
 
 int main(int argc, char *argv[]) {
 
-
     for(int i=1;i<argc;i++)
-        makeMD5(&argv[i]);
+        makeMD5(argv[i]);
 
+}
+
+
+void makeMD5 (char *argv){
+    const char *filename = argv;
+    FILE *file = fopen(filename, "rb");
+
+    if (!file) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+    char command[256];
+    snprintf(command, sizeof(command), "md5sum %s", filename);
+    printf("Command: %s",command);
+
+    // Ejecuta el comando md5sum
+    //printf("Calculando el hash MD5...\n");
+    int result = system(command);
+
+    // ACA HAY Q ABRIR UN PIPE PARA COMUNICARNOS CON LA CONSOLA
+
+    if (result != 0) {
+        printf("Ocurrió un error al calcular el hash MD5.\n");
+        return;
+    }
+    /*
+    MD5_CTX md5Context;
+    MD5_Init(&md5Context);
+
+    unsigned char buffer[READ_BUFFER_SIZE];
+    size_t bytesRead;
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        MD5_Update(&md5Context, buffer, bytesRead);
+    }
+
+    unsigned char hash[MD5_DIGEST_LENGTH];
+    MD5_Final(hash, &md5Context);
+    // en hash queda guardado el md5
+    fclose(file);
+
+    printf("El MD5 del archivo %s es: ", filename);
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        printf("%02x", hash[i]);
+    }*/
+    printf("\n");
+
+    return;
 }
 
 /*
--- main.c
+++ main.c
@@ -6,26 +6,33 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 #define PROCESSES 2
 
 void callSlave (char * archivo);
+void openFiles(char* path);
+void printFiles(char* path,int tabs);
 
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
         printf("Ingrese un argumento\n");
+        return 1;
         //return 1;
     }
+
+    //openFiles(argv[1]);
+    printFiles(argv[1],0);
+
+
+
     // este for es testing
-    for(int i=0; i<PROCESSES; i++){
-        char * archivo;
-        if(i%2==0)
-            archivo="README.md";
-        else
-            archivo="child.c";
-        callSlave(archivo);
-    }
+    /*for(int i=0; i<PROCESSES; i++){
+        callSlave("README.md");
+    }*/
 
     for (int i = 0; i < PROCESSES; i++) {
         int status;
@@ -35,10 +42,39 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
     printf("Todos los procesos hijos han terminado. Programa principal finalizado.\n");
 
 }
+
+void printFiles(char* path, int tabs){
+    struct stat stat1;
+    struct dirent* dirent1;
+
+    if(tabs>=2)
+        return;
+
+    if (stat(path, &stat1)==-1)
+        return;
+
+    if ((stat1.st_mode & S_IFMT)==S_IFDIR){
+        DIR* aux= opendir(path);
+        while((dirent1=readdir(aux))!=NULL){
+            if ((dirent1)->d_name[0]!='.'){
+                char buff[256];
+                sprintf(buff,"%s/%s",path,dirent1->d_name);
+                printFiles(buff, tabs+1);   // reutilizamos el codigo hecho en el ejer 1 de l guia 0
+            }
+        }
+        //chdir("../");
+        closedir(aux);
+    }else     {
+        printf("%s\n",path);
+        callSlave(path);
+        return;
+    }
+}
+
+
 
 void callSlave (char * archivo){
     pid_t child_pid;
@@ -48,9 +84,6 @@ void callSlave (char * archivo){
         perror("fork");
         exit (1);
     }else if (child_pid ==0){
-
-
-
         char * args[]={"./child",archivo,NULL}; // siempre al final de estos arrays tiene que haber un null
         execve (args[0],args,NULL); // siempre al final de estos arrays tiene que haber un null
         perror("execve");
